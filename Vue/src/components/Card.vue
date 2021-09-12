ff08fbee56baf18521f1581c28c121b719be8f88
Soban Farooq <sobanfarooq3@gmail.com>
2021-09-12 19:23:23 +0500

    Added Vue Draggable library

--- Vue/src/components/Card.vue
+++ Vue/src/components/Card.vue
@@ -1,18 +1,32 @@
 <template>
 <div>
-    <div class="card bg-white mt-3 overflow-auto h-24 rounded-md" v-for="card in cards" :key="card">
-        <p class="font-medium text-gray-600 px-2 py-1 line-clamp-2 hover:line-clamp-none">
-            {{ card }}
-        </p>
-    </div>
+    <draggable :list="tickets">
+        <div class="card bg-white mt-3 overflow-auto h-24 rounded-md" v-for="card in cards" :key="card">
+            <p class="font-medium text-gray-600 px-2 py-1 line-clamp-2 hover:line-clamp-none">
+                {{ card }}
+            </p>
+        </div>
+    </draggable>
 </div>
 </template>
 
 <script>
+import draggable from 'vuedraggable'
 export default {
     name: 'Card',
     props: {
         cards: Array
     },
+    components: {
+        draggable,
+    },
+    data() {
+        return {
+            tickets: [],
+        }
+    },
+    mounted() {
+        this.tickets = this.cards;
+    }
 }
 </script>
